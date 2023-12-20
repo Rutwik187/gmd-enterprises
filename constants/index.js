0fe1e831ce42737e73e2837bdeb79b075ccd9b03
Rutwik187 <rutwikshinde34@gmail.com>
2023-12-20 20:31:12 +0530

    made base setup

---
+++ constants/index.js
@@ -0,0 +1,89 @@
+export const exploreWorlds = [
+  {
+    id: 'Product1',
+    imgUrl: '/prod1.jpg',
+    title: 'Car Oil',
+  },
+  {
+    id: 'product2',
+    imgUrl: '/prod2.jpg',
+    title: 'Engine Oil',
+  },
+  {
+    id: 'product3',
+    imgUrl: '/prod3.jpg',
+    title: 'Automobile Oil',
+  },
+  {
+    id: 'product4',
+    imgUrl: '/prod2.jpg',
+    title: 'Bearing Oil',
+  },
+  {
+    id: 'product5',
+    imgUrl: '/prod1.jpg',
+    title: 'Grease Oil',
+  },
+];
+
+export const startingFeatures = [
+  'lorem ipsum generator online temperory Text',
+  'lorem ipsum generator online temperory Text',
+  'lorem ipsum generator online temperory Text',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+      'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+      'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/cover.jpg',
+    title: 'lorem ipsum generator online temperory Text',
+    subtitle:
+      'lorem ipsum generator online temperory Text.lorem ipsum generator online temperory Text',
+  },
+  {
+    imgUrl: '/cover.jpg',
+    title: 'lorem ipsum generator online temperory Text',
+    subtitle:
+      'lorem ipsum generator online temperory Text.lorem ipsum generator online temperory Text',
+  },
+  {
+    imgUrl: '/cover.jpg',
+    title: 'lorem ipsum generator online temperory Text',
+    subtitle:
+      'lorem ipsum generator online temperory Text.lorem ipsum generator online temperory Text',
+  },
+
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { TypingText } from "../components";
+
+import styles from "../styles";
+import { fadeIn, staggerContainer } from "../utils/motion";
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    {/* <div className="gradient-02 z-0" /> */}
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About GMD Enterprises" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn("up", "tween", 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-bold text-gray-800">
+          Welcome to GMD Enterprises, a pioneering force in lubricant
+          manufacturing where innovation meets excellence. With a commitment to
+          precision and reliability, we pride ourselves on crafting lubricants
+          that surpass industry standards. Our dedicated team of experts
+          combines experience with ingenuity, delivering products that optimize
+          performance and longevity across diverse sectors.
+        </span>
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn("up", "tween", 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
